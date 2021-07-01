8e7c5cc835322bc63701b9a60d412111da68f0f5
Bart Zaalberg <b.zaalberg@simgroep.nl>
2021-07-01 10:52:14 +0200

    Updated edit link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -96,7 +96,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://gitlab.com/brothersonline/xlua-battletalent-docs/-/edit/master/website/'
         },
         blog: {
           showReadingTime: true,
